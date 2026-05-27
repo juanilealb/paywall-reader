446426ca8f96644648d04040143f85c3b7337167
Juan Ignacio Leal Bazterrica <126987726+juanilealb@users.noreply.github.com>
2026-05-27 19:23:22 -0300

    feat: add in-page reader mode (#16)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.juani.paywallreader"
         minSdk = 26
         targetSdk = 36
-        versionCode = 14
-        versionName = "1.0-update14"
+        versionCode = 15
+        versionName = "1.0-update15"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
