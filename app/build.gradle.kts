f718287fb0c3baff6f9302b8e397ebc381eae946
juanilealb <126987726+juanilealb@users.noreply.github.com>
2026-05-28 10:31:19 +0000

    chore: bump Android test build to update22

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.juani.paywallreader"
         minSdk = 26
         targetSdk = 36
-        versionCode = 21
-        versionName = "1.0-update21"
+        versionCode = 22
+        versionName = "1.0-update22"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
